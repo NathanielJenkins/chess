148b4bfe1fd940fd7e78218ba79a369c480440e0
NathanielJenkins <njboale@gmail.com>
2020-08-16 21:40:33 -0700

    Updates

---
+++ precache-manifest.8b4ad22e515b478f98ce42665c261151.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5cbec5aa0be13b8c773c5e241e2efc",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "66169aa7a7a19acfa94f",
+    "url": "/chess/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "66169aa7a7a19acfa94f",
+    "url": "/chess/static/js/2.e3b590f4.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/chess/static/js/2.e3b590f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "618607bec8fd74e267b6",
+    "url": "/chess/static/js/main.2d560ba8.chunk.js"
+  },
+  {
+    "revision": "fe54e1bce9cc9d1a3729",
+    "url": "/chess/static/js/runtime-main.bff3f106.js"
+  }
+]);
